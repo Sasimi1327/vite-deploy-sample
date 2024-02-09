e39074cf4a09f7db81148a799fae5a7d2710b815
sasimi1327 <wonderland1327@gmail.com>
2024-02-09 21:09:58 +0800

    first commit

---
+++ src/views/AboutView.vue
@@ -0,0 +1,38 @@
+<script>
+export default {
+  data() {
+    return {
+      user: {}
+    }
+  },
+  methods: {
+    getUser() {
+      console.log(import.meta.env.VITE_TEXT);
+      const pathUrl = import.meta.env.VITE_PATH;
+      this.$http.get(pathUrl).then((res) => {
+        this.user = res.data.results[0];
+        console.log(this.user)
+      })
+    }
+  },
+  mounted() {
+    this.getUser()
+  }
+}
+</script>
+
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+  </div>
+</template>
+
+<style>
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
